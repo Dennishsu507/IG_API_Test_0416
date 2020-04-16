db570557753b9f781a35c75b6846385e36ce7cfc
hongweihsu <dennishsu@arch.nctu.edu.tw>
2020-04-16 17:22:05 +0800

    Initial Commit

---
+++ IG_API_Test_0416/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IG_API_Test_0416
+//
+//  Created by HSU, Hong-Wei on 2020/4/16.
+//  Copyright © 2020 HSU, Hong-Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
